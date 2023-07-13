3f0d0e1dd2690d979153f6462f500034e1625952
Gerson116 <gerson.santosm3@gmail.com>
2023-07-13 19:48:34 -0400

    Se organizo la maqueta de manera basica en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    HomeComponent,
+    ResumeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css', '../../../assets/css/style.css']
+})
+export class HeaderComponent {
+
+  constructor(){
+    //...
+  }
+
+}
